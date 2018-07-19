f2ba71df44db674d565bda6d896c3a6bf18c46c8
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-19 15:22:47 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_selector :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.each do |word|
+      if word.split('').sort == self.word.split('').sort
+        return word
+      end
+    end
+
+
+end
